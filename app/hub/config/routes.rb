ed00e5ea1440a93e5cb888eca50ed7c1957f14ba
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2024-07-12 12:46:36 +1200

    Routing fix

--- app/hub/config/routes.rb
+++ app/hub/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root 'tasks#index'
 
-  resource :account, only: :create, controller: :account
+  resource :account, only: :create, controller: :account do
+    resource :login, only: [:show, :create], to: 'account#login'
+  end
 
   get  ':controller(/:action(/:id))(.:format)'
   post ':controller(/:action(/:id))(.:format)'
