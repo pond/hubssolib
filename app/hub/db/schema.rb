17fe9ef20c9ea868a09e39b3b8428b1c8b2f655f
Andrew Hodgkinson <andrew@ripaglobal.com>
2024-11-26 10:54:12 +1300

    Don't force-create table; slightly modernise schema

--- app/hub/db/schema.rb
+++ app/hub/db/schema.rb
@@ -10,23 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema[7.2].define(version: 3) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "crypted_password", limit: 40
-    t.string "salt", limit: 40
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string "remember_token"
-    t.datetime "remember_token_expires_at"
-    t.string "activation_code", limit: 40
-    t.datetime "activated_at"
-    t.string "roles"
-    t.integer "member_id"
-    t.string "password_reset_code", limit: 40
-    t.datetime "password_reset_code_expires_at"
-    t.string "real_name", limit: 128
+  create_table "schema_info", id: false, force: :cascade do |t|
+    t.integer "version"
   end
 
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.text "email"
+    t.string "crypted_password", limit: 40
+    t.string "salt", limit: 40
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
+    t.string "remember_token", limit: 255
+    t.datetime "remember_token_expires_at", precision: nil
+    t.string "activation_code", limit: 40
+    t.datetime "activated_at", precision: nil
+    t.text "roles"
+    t.integer "member_id"
+    t.string "password_reset_code", limit: 40
+    t.datetime "password_reset_code_expires_at", precision: nil
+    t.string "real_name", limit: 128
+  end
 end
