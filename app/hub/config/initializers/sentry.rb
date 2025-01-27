e7d4a637481f9c210c9860c08b03e79b6189f382
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2025-01-28 09:57:57 +1300

    Try alternative env config

--- app/hub/config/initializers/sentry.rb
+++ app/hub/config/initializers/sentry.rb
@@ -8,8 +8,9 @@ end
 
 if ENV['SENTRY_DSN'].present?
   Sentry.init do |config|
-    config.dsn                = ENV['SENTRY_DSN']
-    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+    config.dsn                  = ENV['SENTRY_DSN']
+    config.enabled_environments = [Rails.env]
+    config.breadcrumbs_logger   = [:active_support_logger, :http_logger]
 
     # Set traces_sample_rate to 1.0 to capture 100% of transactions for tracing.
     # Set profiles_sample_rate to profile 100% of sampled transactions.
