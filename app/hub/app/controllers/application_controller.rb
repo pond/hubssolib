b226b23639d75729072107259259014fafc859cf
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2025-02-15 20:59:04 +1300

    Fix up e-mail host issues

--- app/hub/app/controllers/application_controller.rb
+++ app/hub/app/controllers/application_controller.rb
@@ -58,23 +58,15 @@ class ApplicationController < ActionController::Base
   #
   private
 
-    # TODO: Happily this is all now outdated, with mailers using views etc.
-    # TODO: Fix ASAP!
+    # Hackery run on a global before-action.
     #
-    # Rather annoyingly, ActionMailer templates have no knowledge of the context
-    # in which they are invoked, unlike normal view templates. This is strange
-    # and, at least for Hub, unhelpful. We could insist that the system installer
-    # configures some static value for the default host for links, but that's a
-    # horrible kludge - once the application is running it always knows its host
-    # via the "request" object.
-    #
-    # This filter patches around this Rails hiccup by wasting a few CPU cycles on
-    # auto-setup of the e-mail host.
+    # ActionMailer doesn't know the host under which an application runs, since
+    # it isn't handling a request. But Hub never sends any e-mail without a
+    # request being handled first, so we can set it here. It's wasteful, but it
+    # works.
     #
     def set_email_host
-      unless ( ActionMailer::Base.default_url_options.has_key?( :host ) )
-        ActionMailer::Base.default_url_options[ :host ] = request.host_with_port
-      end
+      Rails.application.config.action_mailer.default_url_options[:host] = request.host_with_port
     end
 
 end
