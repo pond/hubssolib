ccee8ff4819e67e663df76f67d9f3ff075e3a022
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2025-04-08 15:06:57 +1200

    Moved to GitLab

--- app/hub/app/mailers/application_mailer.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: EMAIL_ADMIN
-  layout 'mailer'
-end
--- app/hub/db/migrate/003_add_real_name.rb
+++
@@ -1,9 +0,0 @@
-class AddRealName < ActiveRecord::Migration[5.2]
-  def self.up
-    add_column "users", "real_name", :string, :limit => 128
-  end
-
-  def self.down
-    remove_column "users", "real_name"
-  end
-end
